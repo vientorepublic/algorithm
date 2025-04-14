f5954e756c12a6b4de3935b96cacdf928629dfb4
vientorepublic <markgod0419@gmail.com>
2025-04-14 11:20:37 +0900

    Add course 8

---
+++ course/8/01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  int x = 333;
+  char str[10] = "abc";
+
+  printf("0123456789\n");
+  printf("----------\n");
+
+  printf("%d\n", x);
+  printf("%5d\n", x);
+  printf("%05d\n", x);
+
+  printf("%s\n", str);
+  printf("%5s\n", str);
+  
+  return 0;
+}
---
+++ course/8/02.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  for (int i = 1; i <= 9; i++) {
+    printf("2 * %2d = %d\n", i, i * 2);
+  }
+  return 0;
+}
