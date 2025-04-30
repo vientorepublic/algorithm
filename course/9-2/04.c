28374d3583facb1ac7fb02d570fb4d032dd6a177
vientorepublic <markgod0419@gmail.com>
2025-04-30 21:00:23 +0900

    Add course 9-2, 04

---
+++ course/9-2/04.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void intro();
+void println_stars();
+void get_int(int *num);
+
+void intro() { printf("=== 두 정수를 더하는 프로그램 ===\n"); }
+
+void println_stars() { printf("**************\n"); }
+
+void get_int(int *num) {
+  printf("정수>> ");
+  scanf("%d", num);
+}
+
+int main() {
+  int a, b;
+  intro();
+  println_stars();
+  get_int(&a);
+  get_int(&b);
+  println_stars();
+  printf("a + b = %d\n", a + b);
+  return 0;
+}
