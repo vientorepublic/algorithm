bf93ef9b7133f24b32414f29ba25415b59ab0af4
vientorepublic <markgod0419@gmail.com>
2025-04-30 20:39:00 +0900

    Add pointer initalization

--- pointer/allocation.c
+++ pointer/allocation.c
@@ -3,7 +3,11 @@
 #include <string.h>
 
 int main() {
-  char *str = (char *)malloc(100 * sizeof(char));
+  // NULL 포인터로 초기화
+  char *str = NULL;
+  // 문자열 메모리 동적 할당
+  // char = 1byte, 1 * 100 = 100byte
+  str = (char *)malloc(100 * sizeof(char));
   if (str == NULL) {
     fprintf(stderr, "Memory allocation failed\n");
     return 1;
@@ -11,8 +15,14 @@ int main() {
   strcpy(str, "Hello, World!");
   printf("%s\n", str);
   free(str);
+  str = NULL;
+  printf("str = %p\n", str);
 
-  int *pi = (int *)malloc(10 * sizeof(int));
+  // NULL 포인터로 초기화
+  int *pi = NULL;
+  // 정수 메모리 동적 할당
+  // int = 4byte, 4 * 10 = 40byte;
+  pi = (int *)malloc(10 * sizeof(int));
   if (pi == NULL) {
     fprintf(stderr, "Memory allocation failed\n");
     return 1;
@@ -21,7 +31,7 @@ int main() {
   *pi = 20;
   printf("간접 연산 수행 후 = %d\n", *pi);
   free(pi);
-  pi = NULL;  // Avoid dangling pointer
+  pi = NULL;
   printf("pi = %p\n", pi);
   return 0;
 }
